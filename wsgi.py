e942fb8be0942ef5cff3c16239eaed4e4bb3c256
Rakesh Porika <rakeshporika66@gmail.com>
2026-06-17 12:07:14 +0530

    Add files via upload

---
+++ wsgi.py
@@ -0,0 +1,15 @@
+"""
+WSGI config for dynamic_generative_residual
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dynamic_generative_residual.settings')
+application = get_wsgi_application()
